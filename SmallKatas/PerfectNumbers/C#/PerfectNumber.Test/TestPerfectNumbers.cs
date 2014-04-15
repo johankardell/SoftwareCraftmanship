de1fc7d9724f086a849650bd20445be18de8568f
Johan Kardell <johan.kardell@gmail.com>
2014-04-15 22:22:29 +0200

    Added more test cases

--- SmallKatas/PerfectNumbers/C#/PerfectNumber.Test/TestPerfectNumbers.cs
+++ SmallKatas/PerfectNumbers/C#/PerfectNumber.Test/TestPerfectNumbers.cs
@@ -7,7 +7,20 @@ namespace PerfectNumber.Test
     public class TestPerfectNumbers
     {
         [TestMethod]
-        public void SixReturnsTrue()
+        public void FourIsNotAPerfectNumber()
+        {
+            // Arrange
+            var perfectNumberCalculator = new PerfectNumberCalculator();
+
+            // Act
+            var actual = perfectNumberCalculator.IsPerfectNumber(4);
+
+            // Assert
+            Assert.IsFalse(actual, "4 is not a perfect number");
+        }
+
+        [TestMethod]
+        public void SixIsAPerfectNumber()
         {
             // Arrange
             var perfectNumberCalculator = new PerfectNumberCalculator();
@@ -20,7 +33,7 @@ namespace PerfectNumber.Test
         }
 
         [TestMethod]
-        public void TenReturnsFalse()
+        public void TenIsNotAPerfectNumber()
         {
             // Arrange
             var perfectNumberCalculator = new PerfectNumberCalculator();
@@ -31,5 +44,97 @@ namespace PerfectNumber.Test
             // Assert
             Assert.IsFalse(actual, "10 is not a perfect number");
         }
+
+        [TestMethod]
+        public void TwentyIsNotAPerfectNumber()
+        {
+            // Arrange
+            var perfectNumberCalculator = new PerfectNumberCalculator();
+
+            // Act
+            var actual = perfectNumberCalculator.IsPerfectNumber(20);
+
+            // Assert
+            Assert.IsFalse(actual, "20 is not a perfect number");
+        }
+
+        [TestMethod]
+        public void TwentyEightIsAPerfectNumber()
+        {
+            // Arrange
+            var perfectNumberCalculator = new PerfectNumberCalculator();
+
+            // Act
+            var actual = perfectNumberCalculator.IsPerfectNumber(28);
+
+            // Assert
+            Assert.IsTrue(actual, "28 is a perfect number");
+        }
+
+        [TestMethod]
+        public void OneHundredTwelveIsNotAPerfectNumber()
+        {
+            // Arrange
+            var perfectNumberCalculator = new PerfectNumberCalculator();
+
+            // Act
+            var actual = perfectNumberCalculator.IsPerfectNumber(112);
+
+            // Assert
+            Assert.IsFalse(actual, "112 is not a perfect number");
+        }
+
+        [TestMethod]
+        public void FourhundredNinetySixIsAPerfectNumber()
+        {
+            // Arrange
+            var perfectNumberCalculator = new PerfectNumberCalculator();
+
+            // Act
+            var actual = perfectNumberCalculator.IsPerfectNumber(496);
+
+            // Assert
+            Assert.IsTrue(actual, "496 is a perfect number");
+        }
+
+        [TestMethod]
+        public void FourThousandOneHundredTwentyNineIsNotAPerfectNumber()
+        {
+            // Arrange
+            var perfectNumberCalculator = new PerfectNumberCalculator();
+
+            // Act
+            var actual = perfectNumberCalculator.IsPerfectNumber(4129);
+
+            // Assert
+            Assert.IsFalse(actual, "4129 is not a perfect number");
+        }
+
+        [TestMethod]
+        public void EightThousandOneHundredTwentyEightIsAPerfectNumber()
+        {
+            // Arrange
+            var perfectNumberCalculator = new PerfectNumberCalculator();
+
+            // Act
+            var actual = perfectNumberCalculator.IsPerfectNumber(8128);
+
+            // Assert
+            Assert.IsTrue(actual, "8128 is a perfect number");
+        }
+
+        [TestMethod]
+        public void TwelveThousandFourHundredElevenIsNotAPerfectNumber()
+        {
+            // Arrange
+            var perfectNumberCalculator = new PerfectNumberCalculator();
+
+            // Act
+            var actual = perfectNumberCalculator.IsPerfectNumber(12411);
+
+            // Assert
+            Assert.IsFalse(actual, "12411 is not a perfect number");
+        }
+
     }
 }
