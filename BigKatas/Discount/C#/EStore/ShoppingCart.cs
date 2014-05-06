e120f5f06993e8730416f78d95ddae8f84df1b2c
Johan Kardell <johan.kardell@gmail.com>
2014-05-06 19:33:27 +0200

    Added C# code for Discound

---
+++ BigKatas/Discount/C#/EStore/ShoppingCart.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EStore
+{
+    public class ShoppingCart
+    {
+        public List<ShoppingCartItem> Items { get; set; }
+
+        public ShoppingCart ()
+    	{
+            Items = new List<ShoppingCartItem>();
+	    }
+
+        public void Add(ShoppingCartItem item)
+        {
+            Items.Add(item);
+        }
+
+        public double Sum()
+        {
+            return 0;
+        }
+    }
+}
---
+++ BigKatas/Discount/C#/EStore/ShoppingCartItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EStore
+{
+    public class ShoppingCartItem
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+    }
+}
